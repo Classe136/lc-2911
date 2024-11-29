fb30217e7acb2d630e9f615eeb10b3aea525e9f5
klelia <clelia.fradella@gmail.com>
2024-11-29 10:32:40 +0100

    scaffolding

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log(__filename);
+/*
+Variabili Globali
+ __dirname, __filename
+
+- require: funzione per usare moduli *(CommonJs) - Per ES6 import/export specificare ina pagckage.json, type: module or rename file extension in .mjs * 
+- module: info sul modulo corrente
+- process : info relative all'ambiente di esecuzione
+
+
+Moduli 
+-built-in
+-esterni di terze parti
+*/
